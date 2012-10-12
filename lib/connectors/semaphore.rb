e6cdbbf1c9461f917b014a3c1ce08bd33e8b8d77
Max Prokopiev <max-prokopiev@yandex.ru>
2012-10-12 17:57:07 +0400

    Basic semaphore support

--- lib/connectors/semaphore.rb
+++ lib/connectors/semaphore.rb
@@ -1,9 +1,19 @@
 module ApiProxy
   class Semaphore < BaseApiProxy
-    def self.process(params)
-    end
+    API_ENDPOINTS = {
+      'branches' => 'https://semaphoreapp.com/api/v1/projects/{{project_token}}/branches?auth_token={{auth_token}}',
+      'branch_status' => 'https://semaphoreapp.com/api/v1/projects/{{project_token}}/{{branch_id}}/status?auth_token={{auth_token}}'
+    }
 
-    def self.template_path(template)
+    def self.process(params)
+      request_params = config[:request_params].merge(
+        project_token: config[:projects][params[:project]],
+        branch_id: params[:branch_id]
+      )
+      url = Mustache.render(API_ENDPOINTS[params[:template]], request_params)
+      puts url
+      response = HTTParty.get url
+      response.body
     end
   end
 end
